4a0764bf82277b4f6159b313f1d61be7c2280155
SERVIDOR_PRODUCAO <alexandre@200horas.com.br>
2018-08-02 21:49:14 -0300

    XX

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 include "autoload.php";
 
-$f = new Facebook("myka_sk8_@hotmail.com","");
-
-var_dump($f->processarMensagens());
+$f = new Facebook("4197241258","22082208");
+$f->processarMensagens();
 
 ?>
