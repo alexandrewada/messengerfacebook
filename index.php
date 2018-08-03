0c35272b27f3d5335692b0439d92a2a5b041c826
Alexandre Wada <30277207+alexandrewada@users.noreply.github.com>
2018-08-02 21:50:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 include "autoload.php";
 
-$f = new Facebook("4197241258","22082208");
+#$f = new Facebook("4197241258","22082208");
 $f->processarMensagens();
 
 ?>
